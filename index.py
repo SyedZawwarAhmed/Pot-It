40fda09684cb02aed308c08d11443a92d755c6a1
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-10-01 14:51:15 +0500

    feat: add more levels

--- index.py
+++ index.py
@@ -109,12 +109,41 @@ shouldSetDirection = False
 score = 0
 ball = Ball(*pos)
 obstacle_color = (58.8, 29.4, 0)
-obstacles = [[Obstacle(650, 500, 450, 30)], [Obstacle(250, 500, 450, 30), Obstacle(850, 400, 450, 30)]]
-level = 1
+obstacles = []
+# Level 1
+obstacles.append([Obstacle(650, 500, 450, 30), Obstacle(350, 300, 250, 30)])
+
+# Level 2
+obstacles.append([Obstacle(250, 500, 450, 30), Obstacle(850, 400, 450, 30), Obstacle(450, 200, 350, 30)])
+
+# Level 3
+obstacles.append([Obstacle(200, 550, 400, 30), Obstacle(750, 350, 350, 30), Obstacle(350, 150, 450, 30)])
+
+# Level 4
+obstacles.append([Obstacle(300, 450, 350, 30), Obstacle(600, 300, 400, 30), Obstacle(900, 400, 300, 30)])
+
+# Level 5
+obstacles.append([Obstacle(150, 50, 450, 30), Obstacle(950, 700, 350, 30), Obstacle(550, 450, 350, 30)])
+
+# Level 6
+obstacles.append([Obstacle(400, 250, 350, 30), Obstacle(800, 350, 450, 30), Obstacle(250, 450, 350, 30)])
+
+# Level 7
+obstacles.append([Obstacle(200, 450, 400, 30), Obstacle(600, 100, 350, 30), Obstacle(950, 300, 350, 30)])
+
+# Level 8
+obstacles.append([Obstacle(350, 350, 450, 30), Obstacle(750, 450, 350, 30), Obstacle(450, 50, 400, 30)])
+
+# Level 9
+obstacles.append([Obstacle(100, 150, 400, 30), Obstacle(900, 300, 450, 30), Obstacle(300, 400, 350, 30)])
+
+# Level 10
+obstacles.append([Obstacle(250, 250, 350, 30), Obstacle(700, 600, 400, 30), Obstacle(150, 500, 450, 30)])
+level = 3
 hole = Hole(800, 200, 60, 60)
 
 obstaclesList = []
-max_level = 2
+max_level = 10
 retry_button = RetryButton(screen_width - 150, screen_height - 150, 100, 100, "retry.png")
 
 while running:
@@ -182,7 +211,7 @@ while running:
 
     for obstacle in obstaclesList:
         obstacle_rect = obstacle.rect
-        check_collision(ball_rect, obstacle_rect, 25)
+        check_collision(ball_rect, obstacle_rect, 50)
 
     if not isBallMoving or ball.speed <= 0:
         retry_button.draw(screen)
